c14eb37f3450e3cddff299ce67fd4060a4e483f1
Andrey Visniewski <cortaesai@gmail.com>
2023-03-29 21:51:49 -0300

    chore: (Components) - layout da pagina principal

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,6 +1,11 @@
 import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
+import utilStyles from '../styles/utils.module.css';
 
+const profileImage = "../../images/profile.jpg"
 const name = 'Andrey Rannerson Visniewski';
+export const siteTitle = 'Andreys Blog';
 
 export default function Layout({
   children,
@@ -10,13 +15,47 @@ export default function Layout({
   home?: boolean
 }) {
   return (
-    <div className="container">
+    <div className="styles.container">
       <Head>
         <meta 
           name='blog'
           content='Pagina inicial do site pessoal'
           />
+        <meta name="og:title" content={siteTitle} />
       </Head>
+      <header className='styles.header'>
+        {home ? (
+          <>
+            <Image
+              priority
+              src={profileImage}
+              className={utilStyles.borderCircle}
+              height={144}
+              width={144}
+              alt={name}
+              />
+            <h1 className={utilStyles.heading2xl}>{name}</h1>
+          </>
+        ) : (
+          <>
+            <Link href="/">
+              <Image
+                priority
+                src={profileImage}
+                className={utilStyles.borderCircle}
+                height={108}
+                width={108}
+                alt={name}
+                />
+            </Link>
+            <h2>
+              <Link href="/" className={utilStyles.colorInherit}>
+                {name}
+              </Link>
+            </h2>
+          </>
+        )}
+      </header>
     </div>
   )
 }
