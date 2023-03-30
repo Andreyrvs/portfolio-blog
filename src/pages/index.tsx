5e8f7b8744c43cb93aa9cfc571798ac16e3b3c12
Andrey Visniewski <cortaesai@gmail.com>
2023-03-29 22:28:27 -0300

    adiciona lib para gerencial o markdown post

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import Link from 'next/link'
+import { GetStaticProps } from 'next'
+import { getSortedPostsData } from '../lib/posts'
 
 const presentation1 = "Olá, Eu sou Andrey. Tenho interesse em atuar como desenvolvedor Web, diante disso estou em processo de transição de carreira tendo atuado no setor público."
 
@@ -24,7 +27,27 @@ export default function Home({
       <section className={utilStyles.headingMd}>
         <p>{presentation1} {presentation2}</p>
       </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>{title}</Link>
+              <br />
+            </li>
+          ))}
+        </ul>
+      </section>
     </Layout>
 
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
 }
