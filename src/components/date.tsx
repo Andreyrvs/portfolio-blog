127c9bb746ac980f478936c7cb0e1981f70bab0d
Andrey Visniewski <cortaesai@gmail.com>
2023-03-31 12:59:16 -0300

    fix: (Components) Date - Adiciona hora junto a data

--- src/components/date.tsx
+++ src/components/date.tsx
@@ -2,5 +2,5 @@ import { parseISO, format } from 'date-fns'
 
 export default function Date({ dateString }: { dateString: string }) {
   const date = parseISO(dateString)
-  return <time dateTime={dateString}>{format(date, 'd LLLL, yyyy')}</time>
+  return <time dateTime={dateString}>{format(date, 'd LLLL, yyyy p')}</time>
 }
