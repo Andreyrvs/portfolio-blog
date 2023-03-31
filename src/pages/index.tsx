28c52a48ebc58b001803d30b160c207ac8aea2de
Andrey Visniewski <cortaesai@gmail.com>
2023-03-31 13:15:26 -0300

    fix: (App) - aprensentação

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ import { getSortedPostsData } from '../lib/posts'
 import Date from '../components/date'
 
 const presentation1 =
-  'Olá, Eu sou Andrey. Tenho interesse em atuar como desenvolvedor Web, diante disso estou em processo de transição de carreira tendo atuado no setor público.'
+  'Olá, Eu sou Andrey. Estou buscando uma oportunidade na área de desenvolvimento web, por isso estou em processo de transição de carreira tendo atuado no setor público.'
 
 const presentation2 =
   'Decidi me especializar na área de tecnologia após alguns anos trabalhando em serviços braçais que eram repetitivos e exigiam pouco conhecimento. Agora, estou animado para me desenvolver em tecnologias como TypeScript, Next.js, para solucionar desafios em minha carreira.'
