98728f7ebbdd5bd2a4ccd321ae6d8a63b0b131dc
Andrey Visniewski <cortaesai@gmail.com>
2023-04-14 20:43:06 -0300

    chore: prettier

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,13 @@
-import type { AppProps } from 'next/app'
-import { Analytics } from '@vercel/analytics/react'
-import '@/styles/globals.css'
-
+import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
+import "@/styles/globals.css";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
       <Analytics />
     </>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
