cdfd59a581c95d72202bc741263f62d1d8665742
Andrey Visniewski <cortaesai@gmail.com>
2023-04-14 20:19:21 -0300

    fix: (Utlis) - remove exportação default. Adiciona exportacão em cada item

--- src/utils/text.ts
+++ src/utils/text.ts
@@ -1,9 +1,4 @@
-const presentation1 =
+export const presentation1 =
   "Olá, Eu sou Andrey. Estou buscando uma oportunidade na área de desenvolvimento web, por isso estou em processo de transição de carreira tendo atuado no setor público.";
-const presentation2 =
+export const presentation2 =
   "Decidi me especializar na área de tecnologia após alguns anos trabalhando em serviços braçais que eram repetitivos e exigiam pouco conhecimento. Agora, estou animado para me desenvolver em tecnologias como TypeScript, Next.js, para solucionar desafios em minha carreira.";
-
-export default {
-  presentation1,
-  presentation2,
-};
