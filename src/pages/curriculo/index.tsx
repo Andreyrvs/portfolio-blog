f6f2339cc51da8190d5fa44179bec227474c4ac3
Andrey Visniewski <cortaesai@gmail.com>
2023-05-11 14:05:55 -0300

    fix: Curriculo (Pages) - move vizualizador de pfd para um componente

--- src/pages/curriculo/index.tsx
+++ src/pages/curriculo/index.tsx
@@ -1,17 +1,11 @@
-import { Viewer, Worker } from "@react-pdf-viewer/core";
-import "@react-pdf-viewer/core/lib/styles/index.css";
-import "@react-pdf-viewer/core/lib/styles/index.css";
 import Layout from "@/components/layout/Layout";
+import FullScreenButtonExample from "@/components/pdfViewer/PdfViewer";
 
 export default function Curriculo() {
   return (
     <Layout>
       <h1>Curriculo</h1>
-      <Worker workerUrl="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.4.120/pdf.worker.min.js">
-        <div style={{ height: "750px" }}>
-          <Viewer fileUrl="/Curriculo-FullStack.pdf" />
-        </div>
-      </Worker>
+      <FullScreenButtonExample fileUrl="/Curriculo-FullStack.pdf" />
     </Layout>
   );
 }
