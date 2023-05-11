38bb752f1a631bb08a09a66da62593898d28f50c
Andrey Visniewski <cortaesai@gmail.com>
2023-05-10 23:17:07 -0300

    chore: (Pages) - lista nome dos projetos

--- src/pages/projetos/index.tsx
+++ src/pages/projetos/index.tsx
@@ -1,9 +1,28 @@
 import Layout from "@/components/layout/Layout";
+import GetGithubRepos from "@/utils/GetGithubRepos";
+import GithubRepo from "@/types/GithubRepo";
 
-export default function Projetos() {
+function Projetos({ repositories }: any) {
+  repositories.map((repo: GithubRepo, index: number) => console.log(repo));
+
+  const handleRepos = repositories.map((repo: GithubRepo, index: number) => (
+    <div key={index}>{repo.name}</div>
+  ));
   return (
     <Layout>
       <h1>Projetos</h1>
+      <div>{handleRepos}</div>
     </Layout>
   );
+}
+
+export default Projetos;
+
+export async function getStaticProps() {
+  const repositories = await GetGithubRepos();
+  return {
+    props: {
+      repositories,
+    },
+  };
 }
