dc359a1a9c799be89f3bd43c7d092e756cdffe11
Andrey Visniewski <cortaesai@gmail.com>
2023-05-11 13:35:51 -0300

    feature: Curriculo (Page) - Adiciona vizualizador de pdf

--- src/pages/curriculo/index.tsx
+++ src/pages/curriculo/index.tsx
@@ -1,10 +1,17 @@
+import { Viewer, Worker } from "@react-pdf-viewer/core";
+import "@react-pdf-viewer/core/lib/styles/index.css";
+import "@react-pdf-viewer/core/lib/styles/index.css";
 import Layout from "@/components/layout/Layout";
 
 export default function Curriculo() {
   return (
     <Layout>
       <h1>Curriculo</h1>
-      <h2>Parte 1</h2>
+      <Worker workerUrl="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.4.120/pdf.worker.min.js">
+        <div style={{ height: "750px" }}>
+          <Viewer fileUrl="/Curriculo-FullStack.pdf" />
+        </div>
+      </Worker>
     </Layout>
   );
 }
