b0879c6e86740b75ac6a7f39285b5cb41aace124
Andrey Visniewski <cortaesai@gmail.com>
2023-05-10 21:20:30 -0300

    fix: Move pagina do blog para uma rota e deixa o Sobre como pagina inicial

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,41 +1,9 @@
-import fs from "fs";
-import path from "path";
-import matter from "gray-matter";
 import Head from "next/head";
-import Link from "next/link";
-import { GetStaticProps } from "next";
-import Layout, { siteTitle } from "@/components/layout";
+import Layout, { siteTitle } from "@/components/layout/Layout";
 import utilStyles from "../styles/utils.module.css";
-import Date from "@/components/date";
 import { presentation1, presentation2 } from "@/utils/text";
-import Props from "@/types/Props";
 
-const POST_ROUTE = "/blog/";
-
-export default function Home({ sortedPosts }: { sortedPosts: Props[] }) {
-  const listItems = sortedPosts.map(
-    ({ frontMatter: { description, tags, date, id, title } }, index) => (
-      <li className={utilStyles.listItem} key={index}>
-        <Link href={POST_ROUTE + id} passHref>
-          {title}
-        </Link>
-        <br />
-        <small>{description}</small>
-        <br />
-        <small>
-          Tags:
-          {tags.map((item: string) => (
-            <b key={item}>{` ${item}, `}</b>
-          ))}
-        </small>
-        <br />
-        <small className={utilStyles.lightText}>
-          <Date dateString={date} />
-        </small>
-      </li>
-    )
-  );
-
+export default function Home() {
   return (
     <Layout home>
       <Head>
@@ -48,40 +16,6 @@ export default function Home({ sortedPosts }: { sortedPosts: Props[] }) {
           {presentation1} {presentation2}
         </p>
       </section>
-      {/* <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>{listItems}</ul>
-      </section> */}
     </Layout>
   );
 }
-
-export const getStaticProps: GetStaticProps = async () => {
-  const files = fs.readdirSync(path.join("src", "posts"));
-  const posts = files.map((filename) => {
-    const mardownWithMeta = fs.readFileSync(
-      path.join("src", "posts", filename)
-    );
-    const { data: frontMatter } = matter(mardownWithMeta);
-    frontMatter.id = filename.split(".")[0];
-
-    return {
-      // id: filename.split(".")[0],
-      frontMatter,
-    };
-  });
-
-  const sortedPosts = posts.sort((a, b) => {
-    if (a.frontMatter.date < b.frontMatter.date) {
-      return 1;
-    } else {
-      return -1;
-    }
-  });
-
-  return {
-    props: {
-      sortedPosts,
-    },
-  };
-};
