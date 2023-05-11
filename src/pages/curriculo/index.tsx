38b2e74210b7d7c6b4a0fd0e6acf3531ebe441ac
Andrey Visniewski <cortaesai@gmail.com>
2023-05-11 15:52:49 -0300

    fix: Curriculo (Pages) - Remove componentes de teste

--- src/pages/curriculo/index.tsx
+++ src/pages/curriculo/index.tsx
@@ -1,15 +1,11 @@
 import Layout from "@/components/layout/Layout";
 import PdfViewer from "@/components/pdfViewer/PdfViewer";
-// import PdfViewerDefault from "@/components/pdfViewer/PdfViewerDefault";
-import ToolbarSlotsExample from "@/components/pdfViewer/PdfViewerTool";
 
 export default function Curriculo() {
   return (
     <Layout>
       <h1>Curriculo</h1>
-      {/* <PdfViewer fileUrl="/Curriculo-FullStack.pdf" /> */}
-      {/* <PdfViewerDefault fileUrl="/Curriculo-FullStack.pdf" /> */}
-      <ToolbarSlotsExample fileUrl="/Curriculo-FullStack.pdf" />
+      <PdfViewer fileUrl="/Curriculo-FullStack.pdf" />
     </Layout>
   );
 }
