dc4b1c14f5e3015d15a5e3b79dec842aef573452
Andrey Visniewski <cortaesai@gmail.com>
2023-05-11 16:52:20 -0300

    chore: Curriculo (Components) - adiciona toolbar ao entrar em fullscreen

--- src/components/pdfViewer/PdfViewer.tsx
+++ src/components/pdfViewer/PdfViewer.tsx
@@ -1,27 +1,40 @@
 import React, { ReactElement } from "react";
-import { Viewer, Worker } from "@react-pdf-viewer/core";
+import { SpecialZoomLevel, Viewer, Worker } from "@react-pdf-viewer/core";
 import {
   toolbarPlugin,
   ToolbarSlot,
   ToolbarProps,
 } from "@react-pdf-viewer/toolbar";
+import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
 import "@react-pdf-viewer/core/lib/styles/index.css";
 import "@react-pdf-viewer/toolbar/lib/styles/index.css";
 import style from "./pdfViewer.module.css";
 
-interface ToolbarSlotsExampleProps {
+interface ToolbarSlotsProps {
   fileUrl: string;
 }
 
-const PdfViewer: React.FC<ToolbarSlotsExampleProps> = ({ fileUrl }) => {
-  const toolbarPluginInstance = toolbarPlugin();
+const PdfViewer: React.FC<ToolbarSlotsProps> = ({ fileUrl }) => {
+  const toolbarPluginInstance = toolbarPlugin({
+    fullScreenPlugin: {
+      // Zoom to fit the screen after entering and exiting the full screen mode
+      onEnterFullScreen: (zoom) => {
+        zoom(SpecialZoomLevel.PageWidth);
+      },
+      onExitFullScreen: (zoom) => {
+        zoom(SpecialZoomLevel.PageWidth);
+      },
+      getFullScreenTarget: (pagesContainer): HTMLElement | any =>
+        pagesContainer.closest('[data-testid="default-layout__body"]'),
+      renderExitFullScreenButton: (props) => <></>,
+    },
+  });
   const { Toolbar } = toolbarPluginInstance;
 
   const renderToolbar = (Toolbar: (props: ToolbarProps) => ReactElement) => (
     <Toolbar>
       {(slots: ToolbarSlot) => {
         const {
-          CurrentPageInput,
           Download,
           EnterFullScreen,
           GoToNextPage,
@@ -32,6 +45,7 @@ const PdfViewer: React.FC<ToolbarSlotsExampleProps> = ({ fileUrl }) => {
           Zoom,
           ZoomIn,
           ZoomOut,
+          CurrentPageLabel,
         } = slots;
         return (
           <>
@@ -54,8 +68,8 @@ const PdfViewer: React.FC<ToolbarSlotsExampleProps> = ({ fileUrl }) => {
               <div>
                 <GoToPreviousPage />
               </div>
-              <div className={style.currentPageInput}>
-                <CurrentPageInput />
+              <div className={style.spaceItens}>
+                <CurrentPageLabel />
               </div>
               <div className={style.spaceItens}>
                 / <NumberOfPages />
@@ -66,7 +80,7 @@ const PdfViewer: React.FC<ToolbarSlotsExampleProps> = ({ fileUrl }) => {
             </section>
 
             <section className={style.container}>
-              <div style={{ padding: "0px 2px", marginLeft: "auto" }}>
+              <div className={`rpv-core__minimal-button ${style.spaceItens}`}>
                 <EnterFullScreen />
               </div>
               <div className={style.spaceItens}>
@@ -84,7 +98,10 @@ const PdfViewer: React.FC<ToolbarSlotsExampleProps> = ({ fileUrl }) => {
 
   return (
     <Worker workerUrl="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.4.120/pdf.worker.min.js">
-      <div className={style.card}>
+      <div
+        className={`rpv-core__viewer ${style.card}`}
+        data-testid="default-layout__body"
+      >
         <div className={style.cardHeader}>{renderToolbar(Toolbar)}</div>
         <div className={style.cardBody}>
           <Viewer fileUrl={fileUrl} plugins={[toolbarPluginInstance]} />
