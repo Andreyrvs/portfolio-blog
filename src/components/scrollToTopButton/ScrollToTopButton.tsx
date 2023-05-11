c487cc5d57c3af188d37d71d09510389a4fdf6d1
Andrey Visniewski <cortaesai@gmail.com>
2023-05-11 19:08:52 -0300

    feature: (App) - Botão aparece quando a pagina é descida 54px para baixo, ao clicar o botão leva ao topo da pagina em 0px

---
+++ src/components/scrollToTopButton/ScrollToTopButton.tsx
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from "react";
+import style from "./scrollToTopButton.module.css";
+import ArrowUp from "/public/arrow-up-340.svg";
+import Image from "next/image";
+
+const MARGIN_TOP = 54;
+const INITIAL_SCROLL = 0;
+
+function ScrollToTopButton() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  function handleScroll() {
+    if (
+      document.body.scrollTop > MARGIN_TOP ||
+      document.documentElement.scrollTop > MARGIN_TOP
+    ) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  function handleClick() {
+    document.body.scrollTop = INITIAL_SCROLL;
+    document.documentElement.scrollTop = INITIAL_SCROLL;
+  }
+
+  const handleVisibility = isVisible && (
+    <button
+      id="myBtn"
+      title="Go to top"
+      onClick={handleClick}
+      className={style.scrollBtn}
+    >
+      <Image
+        src={ArrowUp}
+        alt="Seta apontando para o topo da página"
+        width={75}
+        height={50}
+      />
+    </button>
+  );
+
+  return <>{handleVisibility}</>;
+}
+
+export default ScrollToTopButton;
