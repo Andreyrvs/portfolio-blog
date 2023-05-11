acd352bfadcab7c9044e0e79af47f4920f300259
Andrey Visniewski <cortaesai@gmail.com>
2023-05-11 00:27:36 -0300

    feat: (Components) - Adicona card com stilo que renderiza projetos da trybe via github-api

---
+++ src/components/CardProject/CardProject.tsx
@@ -0,0 +1,20 @@
+import styles from "./CardProject.module.css";
+import GithubRepo from "@/types/GithubRepo";
+
+export default function CardProject({
+  name,
+  description,
+  html_url,
+}: GithubRepo) {
+  return (
+    <div>
+      <div className={styles.items}>
+        <p>{name}</p>
+        <p>{description}</p>
+        <a href={html_url} target="_blank">
+          Repositório
+        </a>
+      </div>
+    </div>
+  );
+}
