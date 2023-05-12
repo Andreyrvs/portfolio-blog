aa44708dcf2cf8c4ad3a3c01854418b02a17f2f9
Andrey Visniewski <cortaesai@gmail.com>
2023-05-12 16:01:10 -0300

    fix: Projetos (Components) - Trata os nomes de repositorios removendo numeros e '-' hifen

--- src/components/cardProject/CardProject.tsx
+++ src/components/cardProject/CardProject.tsx
@@ -7,6 +7,32 @@ export default function CardProject({
   html_url,
   homepage,
 }: GithubRepo) {
+  const handleRepositoriesNames = (name: string) => {
+    if (name.includes("stranger")) {
+      return handleStranger(name);
+    }
+    return handleDefault(name);
+  };
+
+  const handleStranger = (name: string) => {
+    const removeDot = name.replace(/\d+\./g, "");
+    const removeHyphen = removeDot.replace(/[-\d]/g, " ");
+
+    const inserted = removeHyphen.includes("-1")
+      ? removeHyphen.concat(" Backend")
+      : removeHyphen.concat(" Frontend");
+
+    const removeWithSpaces = inserted.trim();
+    return removeWithSpaces;
+  };
+
+  const handleDefault = (name: string) => {
+    const newStr = name.replace(/[-\d]/g, " ");
+
+    const removeWithSpaces = newStr.trim();
+    return removeWithSpaces;
+  };
+
   const handleHomePage = homepage && (
     <a href={homepage} target="_blank" rel="noopener noreferrer">
       Live
@@ -16,7 +42,7 @@ export default function CardProject({
     <>
       <li className={styles.card}>
         <div>
-          <h1 className={styles.header}>{name}</h1>
+          <h1 className={styles.header}>{handleRepositoriesNames(name)}</h1>
           <p className={styles.font}>{description}</p>
         </div>
         <div className={styles.links}>
