1b65aac42b7dc35c3068bffcc7c64f7fd5993cd7
Andrey Visniewski <cortaesai@gmail.com>
2023-05-12 19:34:47 -0300

    fix: (Utils) - Move texto para header

--- src/utils/text.ts
+++ src/utils/text.ts
@@ -1,4 +1,4 @@
 export const presentation1 =
-  "Olá, Eu sou Andrey. Estou buscando uma oportunidade na área de desenvolvimento web, estou em processo de transição de carreira";
+  "Estou buscando uma oportunidade na área de desenvolvimento web, estou em processo de transição de carreira";
 export const presentation2 =
   "Decidi me especializar na área de tecnologia após alguns anos trabalhando em serviços braçais que eram repetitivos e exigiam pouco conhecimento. Agora, estou animado para me desenvolver em tecnologias como TypeScript, Next.js, Prisma, para solucionar desafios em minha carreira.";
