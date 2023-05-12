04c0029cf895a672fd1f8eb2c44be3a9f9a69f02
Andrey Visniewski <cortaesai@gmail.com>
2023-05-12 14:56:07 -0300

    chore: Projetos (Components) CardProject - Adiciona <li> manter a semantica. divide os textos para dispor corretamente no css

--- src/components/cardProject/CardProject.tsx
+++ src/components/cardProject/CardProject.tsx
@@ -7,23 +7,25 @@ export default function CardProject({
   html_url,
   homepage,
 }: GithubRepo) {
-  const handleHomePage = homepage ? (
+  const handleHomePage = homepage && (
     <a href={homepage} target="_blank" rel="noopener noreferrer">
       Live
     </a>
-  ) : (
-    ""
   );
   return (
     <>
-      <div className={styles.items}>
-        <p>{name}</p>
-        <p>{description}</p>
-        <a href={html_url} target="_blank">
-          Repositório
-        </a>
-        <small>{handleHomePage}</small>
-      </div>
+      <li className={styles.card}>
+        <div>
+          <h1 className={styles.header}>{name}</h1>
+          <p className={styles.font}>{description}</p>
+        </div>
+        <div className={styles.links}>
+          <a className={styles.font} href={html_url} target="_blank">
+            Repositório
+          </a>
+          <small>{handleHomePage}</small>
+        </div>
+      </li>
     </>
   );
 }
