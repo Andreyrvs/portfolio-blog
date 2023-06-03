33d4442e031eb60f9db32d0572fbaa80d5e9d9a3
Andrey R. Visniewski <andreyrvdev@gmail.com>
2023-06-03 01:07:28 -0300

    fix: (Components) Header - cores para Darkmode

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -20,32 +20,32 @@ export default function Header() {
       <section className={styles.container}>
         <div className={styles.name_container}>
           <p className={`${jsosefinSans.className} ${styles.name}`}>{name}</p>
-          <div className={styles.iconsContainer}>
-            <a href="https://github.com/Andreyrvs" target="_blank">
-              <Image
-                priority
-                src={githubIcon2}
-                className={styles.icons}
-                height={25}
-                width={25}
-                alt="logo github"
-              />
-            </a>
-            <a href="https://www.linkedin.com/in/andreyrv/" target="_blank">
-              <Image
-                priority
-                src={linkedinIcon}
-                className={styles.icons}
-                height={25}
-                width={25}
-                alt="logo linkedin"
-              />
-            </a>
-          </div>
         </div>
         <h1 className={`${styles.profession} ${jsosefinSans.className}`}>
           {profession}
         </h1>
+        <div className={styles.iconsContainer}>
+          <a href="https://github.com/Andreyrvs" target="_blank">
+            <Image
+              priority
+              src={githubIcon2}
+              className={styles.icons}
+              height={25}
+              width={25}
+              alt="logo github"
+            />
+          </a>
+          <a href="https://www.linkedin.com/in/andreyrv/" target="_blank">
+            <Image
+              priority
+              src={linkedinIcon}
+              className={styles.icons}
+              height={25}
+              width={25}
+              alt="logo linkedin"
+            />
+          </a>
+        </div>
       </section>
     </header>
   );
